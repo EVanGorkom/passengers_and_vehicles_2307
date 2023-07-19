1c0a5d7413a724ff041488b99e20d59b835a255e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-19 11:01:16 -0500

    Create the vehicle file and spec

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,7 +1,22 @@
 class Passenger
+  attr_reader :age, :drive
 
   def initialize(passenger_info)
     @name = passenger_info[:name]
     @age = passenger_info[:age]
+    @drive = nil
   end
+
+  def adult?
+    true if @age >= 18
+  end
+
+  def drive
+    @drive = true
+  end
+
+  def driver?
+    true if @drive == true
+  end
+
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,16 +1,34 @@
-require "./lib/passenger"
+require "./lib/passenger.rb"
 
 RSpec.describe Passenger do
   describe "#initialize" do
     it "can initialize" do
-      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-    end
+      charlie = Passenger.new({name: "Charlie", age: 18})
+      taylor = Passenger.new({name: "Taylor", age: 12})
 
+      expect(charlie).to be_an_instance_of(Passenger)
+    end
+  end
+
+  describe "#adult?" do
     it "has readable attributes" do
-      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-      
+      charlie = Passenger.new({name: "Charlie", age: 18})
+      taylor = Passenger.new({name: "Taylor", age: 12})
+
+      charlie.age
+      charlie.adult?
+      taylor.adult?
+    end
+  end
+
+  describe "#drive?" do
+    it "can determine who is driving" do
+      charlie = Passenger.new({name: "Charlie", age: 18})
+      taylor = Passenger.new({name: "Taylor", age: 12})
+  
+      charlie.driver?
+      charlie.drive
+      charlie.driver?
     end
   end
 end
